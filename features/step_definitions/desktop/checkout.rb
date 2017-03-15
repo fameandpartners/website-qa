cca1bd742bd0c5ca63c48b35f41c87f962aea8e6
Andrey Petrenko <andreypetrenko@thepaydock.com>
2017-03-15 17:22:14 +0200

    dress xpath updated for checkout cart

--- features/step_definitions/desktop/checkout.rb
+++ features/step_definitions/desktop/checkout.rb
@@ -140,7 +140,7 @@ end
 
 Then(/^I can delete the dress from cart\.$/) do
   on(CheckOutPage) do |page|
-    page.span_element(xpath: "//div[contains(@class,'product-form-side')]//a[text()='Kirrily']/../..//span[@class='icon btn-close']")\
+    page.span_element(xpath: "//div[contains(@class,'product-form-side')]//a[text()='Eclectic Love Dress']/../..//span[@class='icon btn-close']")\
 .when_present(30).click
     sleep 3
   end
